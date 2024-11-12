1f11291df76f3ea5c02f2bddf2f8efcd268cf0a5
Master Master <dnevgad@gmail.com>
2024-11-12 18:30:56 +0200

    new

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -21,7 +21,6 @@ class Program
 
     static void Main()
     {
-        // Зчитуємо дані з файлу INPUT.TXT
         var input = File.ReadAllLines("INPUT.TXT");
         var firstLine = input[0].Split();
         int n = int.Parse(firstLine[0]);
@@ -29,7 +28,6 @@ class Program
 
         var items = new List<Item>();
 
-        // Зчитуємо інформацію про кожну річ
         for (int i = 0; i < n; i++)
         {
             var line = input[i + 1].Split();
@@ -38,26 +36,21 @@ class Program
             items.Add(new Item(i + 1, wi, di));
         }
 
-        // Сортуємо речі за дедлайном di (чим раніше дедлайн, тим пріоритетніше)
         items = items.OrderBy(item => item.Di).ToList();
 
         var plan = new List<(long time, int id)>();
         long currentTime = 0;
 
-        // Перебираємо кожну річ та плануємо її сушіння
         foreach (var item in items)
         {
-            // Розрахунок часу, щоб висушити річ на мотузці
             long timeOnLine = item.Wi;
 
-            // Якщо річ може висохнути до дедлайну на мотузці
             if (currentTime + timeOnLine <= item.Di)
             {
                 currentTime += timeOnLine;
             }
             else
             {
-                // Якщо потрібно ставити річ на батарею
                 long timeNeededOnBattery = item.Wi - r;
 
                 if (currentTime + timeNeededOnBattery > item.Di)
@@ -71,7 +64,6 @@ class Program
             }
         }
 
-        // Записуємо план у файл OUTPUT.TXT
         using (var writer = new StreamWriter("OUTPUT.TXT"))
         {
             foreach (var (time, id) in plan)
